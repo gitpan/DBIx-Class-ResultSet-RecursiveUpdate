8f4d9f503d3ded9ca136991d9d64e1da0535af78
Zbigniew Łukasiak <zbigniew@lukasiak.name>
2009-05-15 18:34:38 +0000

    Import of ZBY/DBIx-Class-ResultSet-RecursiveUpdate-v0.007 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-ResultSet-RecursiveUpdate
    gitpan-cpan-version:      v0.007
    gitpan-cpan-path:         ZBY/DBIx-Class-ResultSet-RecursiveUpdate-v0.007.tar.gz
    gitpan-cpan-author:       ZBY
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/ResultSet/RecursiveUpdate.pm
+++ lib/DBIx/Class/ResultSet/RecursiveUpdate.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package DBIx::Class::ResultSet::RecursiveUpdate;
 
-use version; our $VERSION = qv('0.006');
+use version; our $VERSION = qv('0.007');
 
 use base qw(DBIx::Class::ResultSet);
 
@@ -306,14 +306,16 @@ This document describes DBIx::Class::ResultSet::RecursiveUpdate version 0.006
 The functional interface:
 
     my $new_item = DBIx::Class::ResultSet::RecursiveUpdate::Functions::recursive_update({ 
-        id => 1, 
-        owned_dvds => [ 
-        { 
-          title => 'One Flew Over the Cuckoo's Nest' 
-        } 
-        ] 
-      }
-    );
+        resultset => $schema->resultset( 'Dvd' ),
+        updates => {
+            id => 1, 
+            owned_dvds => [ 
+                { 
+                  title => 'One Flew Over the Cuckoo's Nest' 
+                } 
+            ] 
+        }
+    });
 
 
 As ResultSet subclass:
